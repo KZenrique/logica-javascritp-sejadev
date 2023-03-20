0859b5b05537a5789750e4b831fa9821ce5e0492
KZenrique <c.henrique_89@hotmail.com>
2023-03-20 13:24:44 -0300

    criado arquivos

---
+++ 02_variaveis.js
@@ -0,0 +1,9 @@
+let numero = 10;
+let texto = "Palavra";
+
+const numeroOvos = 9;
+if (numeroOvos >= 8) {
+  console.log("Pode levar os ovos!");
+} else {
+  console.log("Não possui ovos disponíveis!")
+}
